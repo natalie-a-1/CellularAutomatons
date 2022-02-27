a95943b66170d2f983a6346e2f43b8cb1a9a3fdb
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-02-27 16:05:05 -0600

    Created the Generation class and passed all tests

--- src/Generation.java
+++ src/Generation.java
@@ -1,4 +1,91 @@
+import java.util.Arrays;
 
 public class Generation {
+	
+private boolean[] cellStates;
 
+	public boolean[] getStates() {
+
+		boolean[] output = new boolean[cellStates.length];
+
+		for (int i = 0; i < cellStates.length; i++) {
+
+			output[i] = cellStates[i];
+		}
+		return output;
+	}
+
+	public boolean getState(int idx) {
+		
+		boolean[] arr = Arrays.copyOf(cellStates, cellStates.length);
+		
+		return arr[idx];
+	}
+
+	// getStates is from Keon
+	public String getStates(char falseSymbol, char trueSymbol) {
+
+		String output = "";
+
+		for (int i = 0; i < size(); i++) {
+
+			if (cellStates[i] == true) {
+
+				output = output + trueSymbol;
+
+			} else {
+
+				output = output + falseSymbol;
+			}
+		}
+		return output;
+	}
+
+	public int size() {
+		
+		boolean[] arr = Arrays.copyOf(cellStates, cellStates.length);
+		
+		return arr.length;
+	}
+
+	public Generation(boolean... states) {
+
+		if (states == null || states.length == 0) {
+
+			boolean[] falseB = { false };
+
+			cellStates = Arrays.copyOf(falseB, falseB.length);
+
+		} else {
+
+			cellStates = Arrays.copyOf(states, states.length);
+		}
+	}
+
+	// Generation (states,trueSymbol) is from Keon
+	public Generation(String states, char trueSymbol) {
+
+		boolean[] f = { false };
+
+		if (states == null || states == "") {
+
+			cellStates = f;
+
+		} else {
+
+			cellStates = new boolean[states.length()];
+
+			for (int i = 0; i < states.length(); i++) { 
+
+				if (states.charAt(i) == trueSymbol) {
+
+					cellStates[i] = true;
+
+				} else {
+
+					cellStates[i] = false;
+				}
+			}
+		}
+	}
 }
