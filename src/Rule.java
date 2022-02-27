888f981e0fc341b1ff5eb796f51ec2280693281a
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-02-27 17:04:43 -0600

    Created all aspects of class excpet GetNeighborhood

--- src/Rule.java
+++ src/Rule.java
@@ -1,4 +1,51 @@
 
-public class Rule {
+public abstract class Rule {
 
+	private int ruleNum;
+	
+	public boolean[] rules = new boolean[8];
+
+	
+	protected Rule(int ruleNum) {
+		
+		this.ruleNum = ruleNum;
+		
+	}
+		
+	
+	public int getRuleNum() {
+		
+		int code = ruleNum;
+		
+		return code;
+		
+	}
+	
+	public abstract boolean[] getNeighborhood(int idx, Generation gen);
+	
+	public abstract boolean evolve(boolean[] neighborhood);
+	
+	public Generation evolve(Generation gen) {
+		
+		boolean[] arr = new boolean[gen.size()];
+
+		// for loop from Aaron Coker
+
+		for (int i = 0; i < gen.size(); i++) {
+
+			arr[i] = evolve(getNeighborhood(i, gen));
+
+		}
+		return new Generation(arr);
+		
+	}
+	
+	public abstract String ruleTableString(char falseSymbol, char trueSymbol);
+	
+	public static boolean[] getNeighborhoodByRadius(int idx, int radius, Generation gen) {
+		
+		return null;
+		
+	}
+	
 }
