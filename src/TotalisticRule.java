5087db3113787ff9de3a6b8c776dc68ae59a5b79
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-02-27 19:12:16 -0600

    Created the Table method

--- src/TotalisticRule.java
+++ src/TotalisticRule.java
@@ -1,9 +1,11 @@
+import java.util.StringJoiner;
 
 public class TotalisticRule extends Rule {
 	
 	public boolean rules[] = new boolean[6];
 
 	protected TotalisticRule(int ruleNum) throws RuleNumException {
+		
 		super(ruleNum);
 		
 		if (ruleNum > 63 || ruleNum < 0) {
@@ -41,8 +43,43 @@ public class TotalisticRule extends Rule {
 
 	@Override
 	public String ruleTableString(char falseSymbol, char trueSymbol) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		char[] holder = new char[6];
+
+        for (int i = 0; i < 6; i++) {
+        	
+            if(rules[6-i-1] == false) {
+            	
+                holder[i] = falseSymbol;
+            }
+            else {
+            	
+                holder[i] = trueSymbol;
+            }
+        }
+		
+		String first = "5";
+		
+		String second = "4";
+		
+		String third = "3";
+		
+		String fourth = "2";
+		
+		String fifth = "1";
+		
+		String sixth = "0";
+		
+		StringJoiner sj = new StringJoiner(" ");
+		sj.add(first);
+		sj.add(second);
+		sj.add(third);
+		sj.add(fourth);
+		sj.add(fifth);
+		sj.add(sixth);
+		
+		String output = sj.toString() + System.lineSeparator() + holder[0] + " " + holder[1] + " " + holder[2] + " " + holder[3] +  " " + holder[4] + " " + holder[5];
+		return output;
 	}
 
 }
