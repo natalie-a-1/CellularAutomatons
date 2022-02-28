1aa9ef64aac24a65c726cb54c8ff3f76996dfb3b
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-02-27 20:30:59 -0600

    Started the creation of Elementary Automaton class

---
+++ src/ElementaryAutomaton.java
@@ -0,0 +1,20 @@
+import java.io.File;
+
+public class ElementaryAutomaton extends Automaton{
+
+	protected ElementaryAutomaton(int ruleNum, Generation initial) {
+		super(ruleNum, initial);
+
+	}
+	
+//	protected ElementaryAutomaton(File filename) {
+//		
+//		
+//	}
+
+	@Override
+	protected Rule createRule(int ruleNum) {
+		rule 
+	}
+
+}
