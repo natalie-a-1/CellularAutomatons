b1fc87fe00f6cb7becdf505dd22c941a05255a25
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-02-27 18:37:24 -0600

    Started the Elementary rule class

---
+++ src/ElementaryRule.java
@@ -0,0 +1,22 @@
+
+public class ElementaryRule extends Rule {
+
+	public ElementaryRule(int ruleNum) {
+		super(ruleNum);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean evolve(boolean[] neighborhood) {		
+		
+	}
+	
+	public boolean[] getNeighborhood(int idx, Generation gen) {
+		
+	}
+	
+	public String ruleTableString(char falseSymbol, char trueSymbol) {
+		return null;
+		
+		
+	}
+}
