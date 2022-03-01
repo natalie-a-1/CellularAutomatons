2a0095a302e014d5b91d45db7bdf49ee54e4b901
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-03-01 17:40:28 -0600

    Formatted and created Rule class javadoc

--- src/Rule.java
+++ src/Rule.java
@@ -1,4 +1,11 @@
-
+/**
+ * The Rule class is an abstract class designed to create new Rule objects 
+ * and use the rule to evolve generations and neighborhoods. 
+ * 
+ * 
+ * @author Natalie hill
+ *
+ */
 public abstract class Rule {
 
 	private int ruleNum;
@@ -12,6 +19,11 @@ public abstract class Rule {
 
 	}
 
+/**
+ * Retrieves the rule number passed to the Rule constructor.
+ * 
+ * @return rule number that was passed to the Rule constructor
+ */
 
 	public int getRuleNum() {
 
@@ -20,10 +32,31 @@ public abstract class Rule {
 		return code;
 
 	}
+	/**
+	 *Retrieves cell states in the neighborhood of the cell with the given index.
+	 * 
+	 * @param idx	index in the given Generation
+	 * @param gen	Generation to be searched
+	 * @return boolean array of cell states according to the given index
+	 */
 
 	public abstract boolean[] getNeighborhood(int idx, Generation gen);
+	
+	/**
+	 * Determines the next state of a cell given the current neighborhood.
+	 * 
+	 * @param neighborhood	boolean array of cell states
+	 * @return expected cell state given the neighborhood passed to the method
+	 */
 
 	public abstract boolean evolve(boolean[] neighborhood);
+	
+	/**
+	 * Creates the next Generation given the current Generation.
+	 * 
+	 * @param gen	Generation to be searched 
+	 * @return expected Generation given the previous generation of cell states
+	 */
 
 	public Generation evolve(Generation gen) {
 
@@ -39,8 +72,25 @@ public abstract class Rule {
 		return new Generation(arr);
 
 	}
+	/**
+	 * Displays all of the rules and their outputs with the ability to change the false
+	 *  and true statements. 
+	 * 
+	 * @param falseSymbol	char that represents the false value
+	 * @param trueSymbol	char that represents the true value
+	 * @return table of rules and its output in the form of a String
+	 */
 
 	public abstract String ruleTableString(char falseSymbol, char trueSymbol);
+
+	/**
+	 * Retrieves the next cell states of an index by the radius' value.
+	 * 
+	 * @param idx	index used to specify start of method
+	 * @param radius	specifies how far to go from index to get other states in generation
+	 * @param gen	Generation of cell states
+	 * @return boolean array of cell states a radius value away from the index
+	 */
 
 	//Collaborated with Keon on getNeighborhoodByRadius
 	public static boolean[] getNeighborhoodByRadius(int idx, int radius, Generation gen) {
@@ -48,7 +98,6 @@ public abstract class Rule {
 
 		boolean [] tempo = new boolean[(2*radius)+1];
 
-		//idx = idx + radius;
 
 			for (int i = -radius; i < radius+1; i++) {
 
