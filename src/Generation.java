1933378ba07e949f5525992a4c4af237ff81cbb6
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-03-01 22:50:07 -0600

    Created a javadoc for Generation

--- src/Generation.java
+++ src/Generation.java
@@ -1,8 +1,21 @@
 import java.util.Arrays;
 
+/**
+ * The Generation class creates each Generation in a given automaton.
+ * 
+ * @author Natalie Hill
+ *
+ */
+
 public class Generation {
-	
-private boolean[] cellStates;
+
+	private boolean[] cellStates;
+
+	/**
+	 * This method creates a new cellStates object with the cellStates values.
+	 * 
+	 * @return a boolean array of the cell states
+	 */
 
 	public boolean[] getStates() {
 
@@ -15,12 +28,28 @@ private boolean[] cellStates;
 		return output;
 	}
 
+	/**
+	 * This method creates a copy of cellStates and uses it to find a given index.
+	 * 
+	 * @param idx index of the private array cellStates
+	 * @return the current state of an index in cellStates
+	 */
+
 	public boolean getState(int idx) {
-		
+
 		boolean[] arr = Arrays.copyOf(cellStates, cellStates.length);
-		
+
 		return arr[idx];
 	}
+
+	/**
+	 * This method creates a String representation of cellStates with given
+	 * false/true symbols.
+	 * 
+	 * @param falseSymbol symbol that represents a false value
+	 * @param trueSymbol  symbol that represents a true value
+	 * @return the current state of an index in cellStates with specified values
+	 */
 
 	// getStates is from Keon
 	public String getStates(char falseSymbol, char trueSymbol) {
@@ -41,12 +70,26 @@ private boolean[] cellStates;
 		return output;
 	}
 
+	/**
+	 * This method keeps track of the length of cellStates.
+	 * 
+	 * @return the length of cellStates
+	 */
+
 	public int size() {
-		
+
 		boolean[] arr = Arrays.copyOf(cellStates, cellStates.length);
-		
+
 		return arr.length;
 	}
+
+	/**
+	 * This constructs a Generation of states. Unless the passed states parameter is
+	 * null or the length is 0, then it creates a boolean array with on index set to
+	 * false value.
+	 * 
+	 * @param states A re-sizable boolean array of cellState values
+	 */
 
 	public Generation(boolean... states) {
 
@@ -62,6 +105,14 @@ private boolean[] cellStates;
 		}
 	}
 
+	/**
+	 * This constructs a Generation of states. Unless the passed states parameter is
+	 * null or the empty, then it sets cellStates to a single false value.
+	 * 
+	 * @param states     A String of cellState values
+	 * @param trueSymbol symbol that represents a true value
+	 */
+
 	// Generation (states,trueSymbol) is from Keon
 	public Generation(String states, char trueSymbol) {
 
@@ -75,7 +126,7 @@ private boolean[] cellStates;
 
 			cellStates = new boolean[states.length()];
 
-			for (int i = 0; i < states.length(); i++) { 
+			for (int i = 0; i < states.length(); i++) {
 
 				if (states.charAt(i) == trueSymbol) {
 
