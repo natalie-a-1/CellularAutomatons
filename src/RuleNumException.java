4861a0894315279eb8886ef66042a19cfac436d9
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-03-01 21:42:34 -0600

    Added javadoc to RuleNumException class

--- src/RuleNumException.java
+++ src/RuleNumException.java
@@ -1,10 +1,24 @@
+/**
+ * This is a subclass of the class Exception. This class throws an exception
+ * when the ruleNum is outside of a given range.
+ * 
+ * 
+ * @author Natalie Hill
+ *
+ */
 
 public class RuleNumException extends Exception {
-	
+
 	private static final long serialVersionUID = 0;
 
+	/**
+	 * 
+	 * @param min the smallest value allowed in a range
+	 * @param max the largest value allowed in a range
+	 */
+
 	public RuleNumException(int min, int max) {
-		
-		super("ruleNum is outside the range " + "[" + min +  ", " + max + "].");
+
+		super("ruleNum is outside the range " + "[" + min + ", " + max + "].");
 	}
 }
