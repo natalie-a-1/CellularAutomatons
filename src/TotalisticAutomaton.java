f70896011e289b25ca92488add6c2aeae09c17f9
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-03-02 16:20:10 -0600

    Created and passed all tests in TotalisticAutomaton class

--- src/TotalisticAutomaton.java
+++ src/TotalisticAutomaton.java
@@ -1,4 +1,26 @@
+import java.io.IOException;
 
-public class TotalisticAutomaton {
+public class TotalisticAutomaton extends Automaton{
+
+	protected TotalisticAutomaton(int ruleNum, Generation initial) throws RuleNumException {
+		
+		super(ruleNum, initial);
+
+	}
+	
+
+	protected TotalisticAutomaton(String filename) throws IOException, RuleNumException {
+
+		super(filename);
+
+	}
+
+	@Override
+	protected Rule createRule(int ruleNum) throws RuleNumException {
+
+		Rule toalisticRule = new TotalisticRule(ruleNum);
+
+		return toalisticRule;
+	}
 
 }
