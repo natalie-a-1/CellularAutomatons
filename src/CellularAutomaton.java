d9f6c24f7b6f44086a44b2118a1c7988f91eb831
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-03-11 22:53:31 -0600

    Final commits

--- src/CellularAutomaton.java
+++ src/CellularAutomaton.java
@@ -1,8 +1,21 @@
+/**
+ * This class was created to create either an ECA or TCA depending in the parameters given.
+ * 
+ * @author nataliehill
+ *
+ */
 
 public enum CellularAutomaton {
 
 	ECA,
 	TCA;
+	
+	/**
+	 * 
+	 * @param s	String of either ECA or TCA value
+	 * @return either an Elementary or Totalistic Cellular Automaton
+	 * @throws CellularAutomatonNotFoundException	this is thrown if either ECA or TCA is not the value of s
+	 */
 	
 	public static CellularAutomaton parse(String s) throws CellularAutomatonNotFoundException {
 		if (s.equalsIgnoreCase("ECA")) { 
--- src/CellularAutomatonNotFoundException.java
+++ src/CellularAutomatonNotFoundException.java
@@ -1,7 +1,19 @@
-
+/**
+ * This is an exception class that is thrown when the Cellular Automaton class
+ * is not given a valid parameter to create a Cellular Automaton.
+ * 
+ * @author nataliehill
+ *
+ */
 public class CellularAutomatonNotFoundException extends Exception {
 
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * This is to throw the message provided.
+	 * 
+	 * @param s	invalid input for Cellular Automaton class 
+	 */
 	
 	public CellularAutomatonNotFoundException(String s) {
 		super("Unknown cellular automaton type " + s);
